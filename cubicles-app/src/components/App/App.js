a82b6d853b76ea609abf37afa5c3746671f1dd5b
Mengjie <mengjie.zhao@tum.de>
2020-11-28 13:33:26 +0100

    structuring

--- cubicles-app/src/components/App/App.js
+++ cubicles-app/src/components/App/App.js
@@ -1,11 +1,13 @@
 import logo from '../../statics/logo.svg';
 import './App.css';
+import Desk from '../Desk/Desk';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Desk/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
