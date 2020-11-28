6aff33e6d6b4d457d083e71934598145f2eda12f
Mengjie Zhao <mengjie.zhao@tum.de>
2020-11-28 15:27:48 +0100

    add icons

--- cubicles-app/src/components/MeetingArea/MeetingArea.js
+++ cubicles-app/src/components/MeetingArea/MeetingArea.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Meeting.css';
+import meeting from '../../statics/meeting.svg';
 
 function MeetingArea() {
   return(
     <div className="meeting" >
+      <img className="meeting_img" src={meeting}/>
       <h1>Meeting Area</h1>
     </div>
   )
