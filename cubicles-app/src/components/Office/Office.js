c9c2b928206b4e76d95e95b24e3dcf703c1913d9
Mengjie <mengjie.zhao@tum.de>
2020-11-28 14:24:20 +0100

    add coffee area

--- cubicles-app/src/components/Office/Office.js
+++ cubicles-app/src/components/Office/Office.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Office.css';
 import Desk from '../Desk/Desk';
-import MeetingArea from '../CoffeeArea/MeetingArea';
+import MeetingArea from '../MeetingArea/MeetingArea';
+import CoffeeArea from '../CoffeeArea/CoffeeArea';
 import Employee from '../Employee/Employee';
 
 
 function Office(){
   return(
     <div className="office" >
+      <CoffeeArea/>
       <div className="two_desk">
         <Desk/>
         <Desk/>
